883d76a705ea048a53b1f9e395267fbae06fbc9b
vivek7325 <68550321+vivek7325@users.noreply.github.com>
2023-01-01 16:43:39 +0530

    Add files via upload

---
+++ jobster/src/pages/Landing.js
@@ -0,0 +1,31 @@
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+          Jobster is an AI platform that matches the right people to the right jobs,
+          saving time for both candidates and recruiters.
+          </p>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
